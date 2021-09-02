573a088f48e5ce8105c105d4cddd99a0dbfb6b8c
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-02 19:26:24 +0530

    created contact object

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,10 +1,14 @@
 package com.main;
 
 public class AddressBookMain {
-
+	
+	public static Contact contact;
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Address Book Program");
+		contact = new Contact("Rahul","Shetty","bypass road","Mangalore","Karnataka","574104","9876543210","shettyrahul@gmail.com");
+		System.out.println("The contact is: "+contact);
 
 	}
 
