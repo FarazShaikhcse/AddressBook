d0e3c3ced555512168abdb9e072922d9e1cfd0c4
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-02 19:46:45 +0530

    multiple contacts can be added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,14 +1,51 @@
 package com.main;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	
 	public static Contact contact;
 	
 	public static void main(String[] args) {
 		
+		int n;
+		Scanner scanner = new Scanner(System.in);
+		
 		System.out.println("Welcome to Address Book Program");
-		contact = new Contact("Rahul","Shetty","bypass road","Mangalore","Karnataka","574104","9876543210","shettyrahul@gmail.com");
-		System.out.println("The contact is: "+contact);
+		System.out.println("Enter the number of contacts to add:");
+		n = scanner.nextInt();
+		
+		Contact[] contacts = new Contact[n];
+		for(int i=0;i<n;i++) //for loop to add n people
+		{
+			contacts[i] = new Contact();
+			System.out.println("Enter first Name:");
+			contacts[i].first_name = scanner.next();
+			System.out.println("Enter last name:");
+			contacts[i].last_name = scanner.next();
+			System.out.println("Enter address:");
+			contacts[i].address = scanner.next();
+			System.out.println("Enter city:");
+			contacts[i].city = scanner.next();
+			System.out.println("Enter state:");
+			contacts[i].state = scanner.next();
+			System.out.println("Enter zip:");
+			contacts[i].zip = scanner.next();
+			System.out.println("Enter phone number");
+			contacts[i].phone_number  = scanner.next();
+			System.out.println("Enter E-mail:");
+			contacts[i].email = scanner.next();
+		}
+		
+		
+		System.out.println("Contact Details are:");
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("Details of person" + i+1);
+			System.out.println(contacts[i]);
+			
+		}
+		
 
 	}
 
--- Contact.java
+++ Contact.java
@@ -11,23 +11,11 @@ public class Contact {
 	public String phone_number;
 	public String email;
 	
-	public Contact(String first_name,String last_name,String address,String city,String state,String zip,String phone_number,String email)
-	{
-		this.first_name=first_name;
-		this.last_name=last_name;
-		this.address=address;
-		this.city=city;
-		this.state=state;
-		this.zip=zip;
-		this.phone_number=phone_number;
-		this.email=email;
-	}
 
 	@Override
 	public String toString() {
-		return "Contact [first_name=" + first_name + ", last_name=" + last_name + ", address=" + address + ", city="
-				+ city + ", state=" + state + ", zip=" + zip + ", phone_number=" + phone_number + ", email=" + email
-				+ "]";
+		return "first_name=" + first_name + ", last_name=" + last_name + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phone_number=" + phone_number + ", email=" + email;
 	}
 	
 
